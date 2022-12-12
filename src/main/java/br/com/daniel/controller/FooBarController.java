18d65cba2e89bc474a63f642ff73b9649b80ef9d
Daniel Celestino <danielcelestino@gmail.com>
2022-12-12 17:22:51 -0300

    Include Circuit Breaker

--- src/main/java/br/com/daniel/controller/FooBarController.java
+++ src/main/java/br/com/daniel/controller/FooBarController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 /*
  * Classe para testar possiveis problemas em ambientes de alta complexidade com microserviços
@@ -23,11 +23,16 @@ public class FooBarController {
 
 	@GetMapping("/foo-bar")
 //	@Retry(name = "default") //tenta 3 vezes antes de fechar
-	@Retry(name = "foo-bar-att") //tenta 5 vezes antes de fechar por application.yml
+//	@Retry(name = "foo-bar-att", fallbackMethod = "fallbackMethod1") //tenta 5 vezes antes de fechar por application.yml
+	@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod1") //pode ser testdo com power shell. Ele não envia todas as requisições e acaba filtrando algumas    ------   no power shell:   while(1) {curl http://localhost:8765/book-service/foo-bar; sleep 0.1}
 	public String fooBar() {
 		logger.info("Request to foo-bar is received!");
 		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class); //Provocando erro com endereço inexistente
 //		return "Foo-bar!!!";
 		return "################################################ " + forEntity.getBody();
+	}
+	
+	public String fallbackMethod1(Exception ex) {
+		return "fallbackMethod foo-bar!!!";
 	}
 }
